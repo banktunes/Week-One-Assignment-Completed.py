d4a0b2ccd0787210a2eb699db53ca6d1505cd9e5
banktunes <bankolemmanuel00@gmail.com>
2025-07-03 20:15:47 +0100

    Add files via upload

---
+++ Week One Assignment Completed.py
@@ -0,0 +1,130 @@
+# ================================
+# Classwork Assignment: NaijaTech Intern Challenge
+# ================================
+
+# Scenario:
+# You're a fresh backend intern at **NaijaTech Innovations**, based in Lagos.
+# You're working on backend tasks to practice Python basics.
+
+# -------------------------------
+# 1. Update a Variable
+# -------------------------------
+# You first entered your full name wrongly.
+# Create a variable `full_name` and assign it "Chuka Emeka".
+# Then update it to "Chuka Okeke" and print the new value.
+# No.1 Answer Here
+Fullname="Chuka_Emeka"
+Fullname="chuka_Okeke"
+print(Fullname)
+
+
+
+# -------------------------------
+# 2. Float and Integer
+# -------------------------------
+# Assign 1.75 to `height` and 27 to `age`.
+# Print them in one sentence using f-string.
+# No.2 Answer Here
+
+Height=1.75
+Age=27 
+print(f"I am {Age} years old and my height is {Height} meters.")
+
+
+# -------------------------------
+# 3. Boolean Check
+# -------------------------------
+# Create `is_logged_in` and set it to True.
+# Print the type of `is_logged_in`.
+# No.3 Answer Here
+
+is_logged_in = True
+print(type(is_logged_in))
+
+# -------------------------------
+# 4. String Concatenation
+# -------------------------------
+# Join first and last name using + and print the result.
+# first = "Ada", last = "Okoro"
+# No.4 Answer Here
+
+first = "Ada"
+last = "okoro"
+# Join first and last name using + and print the result.
+Full_name = first + " " + last
+print(Full_name) 
+
+
+
+# -------------------------------
+# 5. Calculate Remainder
+# -------------------------------
+# If you divide 29 by 5, what’s the remainder?
+# Use the modulus operator and print the result.
+# No.5 Answer Here
+
+remainder = 29 % 5 
+print  (remainder)
+
+
+# -------------------------------
+# 6. User Input & Conversion
+# -------------------------------
+# Get two numbers from the user.
+# Convert them to float and print their sum.
+# No.6 Answer Here
+
+Num1 = float(input("3.5"))
+Num2 = float(input("2.5"))
+
+sum_result = Num1 + Num2
+print(f"The sum is {sum_result}")
+
+# -------------------------------
+# 7. Comparison Operators
+# -------------------------------
+# Is 7 greater than or equal to 5? Print the result.
+# No.7 Answer Here
+
+result = 7 >= 5
+print(result)
+
+
+# -------------------------------
+# 8. Logical AND
+# -------------------------------
+# Create `has_id = True`, `has_clearance = True`
+# Use `and` to check if both are True and print the result.
+# No.8 Answer Here
+has_id = True
+has_clearance = True 
+result = has_clearance and has_id 
+print(result) 
+
+
+# -------------------------------
+# 9. Logical OR
+# -------------------------------
+# `has_pass = False`, `knows_manager = True`
+# Use `or` to check if at least one is True. Print result.
+# No.9 Answer Here
+
+has_pass = False
+Knows_manager  = True 
+result = has_pass or Knows_manager
+print(result) 
+
+
+# -------------------------------
+# 10. NOT Operator
+# -------------------------------
+# Create `can_enter = False`
+# Use `not` to reverse the result and print it.
+# No.10 Answer Here
+
+can_enter = False
+print(not can_enter) 
+
+# ================================
+# End of Assignment 🎯
+# ================================
